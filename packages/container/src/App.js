d965869b64715f816ca0a44682104b40ed462b97
Miodrag Rajkovic <miodrag.rajkovic@seavus.com>
2022-07-07 10:45:02 +0200

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from "./components/MarketingApp";
+
+export default function App() {
+  return (
+    <>
+      <h1>Hello from Container</h1>
+      <MarketingApp />
+    </>
+  )
+}
